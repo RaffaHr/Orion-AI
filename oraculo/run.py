9023e505230d5366eea73aa2e707a3fffc1246c2
RaffaHr <raphael.pro.1228@gmail.com>
2024-10-08 20:56:07 -0300

    Update: fly.toml

--- oraculo/run.py
+++
@@ -1,7 +0,0 @@
-# oraculo/run.py
-import os
-import subprocess
-
-def main():
-    # Executa o comando streamlit run no terminal
-    subprocess.run(['streamlit', 'run', 'cohere_integration.py'], check=True)
