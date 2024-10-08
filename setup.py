2225938d2d731de77a3ff7a50fe2b4b81bff09f3
RaffaHr <raphael.pro.1228@gmail.com>
2024-10-08 20:43:58 -0300

    fix: adicionado arquivos para tentativa de deploy no fly.io

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='meu-projeto',
+    version='1.0',
+    install_requires=[
+        'streamlit',
+        'cohere',
+        # Outras dependências
+    ],
+)
